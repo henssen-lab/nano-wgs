6b7f9f416eb4f90004438df6f95f8cc1468837b0
Madalina Giurgiu <madalina.giurgiu@charite.de>
2021-11-19 10:12:44 +0100

    cnv calling

--- cnv/run_cnv.sh
+++ cnv/run_cnv.sh
@@ -14,6 +14,9 @@ fin=${datadir}/ngmlr_hg19.bam
 #sample=CB1008-initial
 sample=$2
 
+datatemp="/fast/users/giurgium_c/scratch/CNV/${sample}"
+mkdir -p $datatemp
+
 scripts_root=/fast/users/giurgium_c/work/nano-wgs/cnv/smurfseq_scripts/scripts/
 scripts_data=/fast/users/giurgium_c/work/nano-wgs/cnv/smurfseq_scripts/data
 scripts_cnv=/fast/users/giurgium_c/work/nano-wgs/cnv/smurfseq_scripts/scripts/cnvAnalysis.R
@@ -24,11 +27,11 @@ export LC_ALL=en_US.UTF-8
 echo "$fin"
 echo "check bam and bed for consistency in chromosome annotation!!"
 
-mkdir -p ${datadir}/cnv 
+mkdir -p ${datadir}/cnv_50k 
 cd ${scripts_root}
-./filterAlnScoreAndQual.py -i $fin  -o $datadir/cnv/unambig_smurf_frags.sam -s 120 -q 1
+./filterAlnScoreAndQual.py -i $fin  -o $datatemp/unambig_smurf_frags.sam -s 120 -q 1
 
-samtools view -H $datadir/cnv/unambig_smurf_frags.sam  |\
+samtools view -H $fin  |\
    sed -e 's/SN:1/SN:chr1/' | sed -e 's/SN:2/SN:chr2/' | \
    sed -e 's/SN:3/SN:chr3/' | sed -e 's/SN:4/SN:chr4/' | \
    sed -e 's/SN:5/SN:chr5/' | sed -e 's/SN:6/SN:chr6/' | \
@@ -43,12 +46,13 @@ samtools view -H $datadir/cnv/unambig_smurf_frags.sam  |\
    sed -e 's/SN:X/SN:chrX/' | sed -e 's/SN:Y/SN:chrY/' | \
    sed -e 's/SN:MT/SN:chrM/' > h.sam
 
-samtools view -@16 -h -O BAM $datadir/cnv/unambig_smurf_frags.sam > $datadir/cnv/unambig_smurf_frags.bam
-samtools reheader h.sam $datadir/cnv/unambig_smurf_frags.bam | samtools view -h -O SAM  - > $datadir/cnv/unambig_smurf_frags_chr.sam
+samtools view -@16 -h -O BAM $datatemp/unambig_smurf_frags.sam > $datatemp/unambig_smurf_frags.bam
+samtools reheader h.sam $datatemp/unambig_smurf_frags.bam | samtools view -h -O SAM  - > $datatemp/unambig_smurf_frags_chr.sam
+#samtools reheader h.sam $fin | samtools view -h -O SAM  - > $datatemp/unambig_smurf_frags_chr.sam
 
-./getBinCounts.py -v -i $datadir/cnv/unambig_smurf_frags_chr.sam  -c ../data/hg19.chrom.sizes -b ../data/bins_5k_hg19.bed -o $datadir/cnv/bin_counts.bed -s ../data/bin_stats.txt
+./getBinCounts.py -v -i $datatemp/unambig_smurf_frags_chr.sam  -c ../data/hg19.chrom.sizes -b ../data/bins_50k_hg19.bed -o $datatemp/bin_counts.bed -s ../data/bin_stats.txt
 
-cd $datadir/cnv/
-Rscript $scripts_cnv $datadir/cnv/bin_counts.bed $sample ${scripts_data}/bins_5k_hg19_gc.txt ${scripts_data}/bins_5k_hg19_exclude.txt
+cd $datadir/cnv_50k/
+Rscript $scripts_cnv $datatemp/bin_counts.bed $sample ${scripts_data}/bins_50k_hg19_gc.txt ${scripts_data}/bins_50k_hg19_exclude.txt
 
-rm $datadir/cnv/unambig_smurf_frags*
+rm $datatemp/unambig_smurf_frags*
